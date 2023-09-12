177595d8d2d472e797aec82aea3540151978a42c
Julián Andrés Rosas Sánchez <julianrosassan@gmail.com>
2023-09-11 19:15:51 -0500

    Working in undefined function findAll

--- api/src/controllers/Videogames/getAllVideogames.js
+++ api/src/controllers/Videogames/getAllVideogames.js
@@ -1,26 +1,22 @@
+require('dotenv').config();
 const axios = require('axios');
 const { Videogame } = require('../../models/Videogame');
 const { Genre } = require('../../models/Genre');
-require('dotenv').config();
-
 const { API_KEY } = process.env;
 
 async function getAllVideogames(req, res, next) {
   try {
     const response = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`);
     const apiGames = response.data.results;
-    
 
-
+    console.log('FUNCIONA')
     const dbGames = await Videogame.findAll({
       include: Genre
     });
 
-    // const allGames = [...apiGames, ...dbGames];
+    const allGames = [...apiGames, ...dbGames];
 
-    // res.json(allGames);
-
-    res.json(dbGames);
+    res.json(allGames);
   } catch (err) {
     next(err);
   }
--- api/src/db.js
+++ api/src/db.js
@@ -1,47 +1,33 @@
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-
-const {
-  DB_USER, DB_PASSWORD, DB_HOST, PORT
-} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, PORT } = process.env;
+//Modelos
+const VideogameModel = require('./models/Videogame');
+const GenreModel = require('./models/Genre');
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${PORT}/pi_videogames`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  logging: false,
+  native: false,
 });
-console.log("Conexión a la base de datos establecida");
-const basename = path.basename(__filename);
 
-const modelDefiners = [];
-
-// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos establecida con éxito.');
+  })
+  .catch(err => {
+    console.error('No se pudo conectar a la base de datos:', err);
   });
 
-// Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
-// Capitalizamos los nombres de los modelos ie: product => Product
-let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
-sequelize.models = Object.fromEntries(capsEntries);
 
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
-const { Videogame, Genre } = sequelize.models;
+const Videogame = VideogameModel(sequelize);
+const Genre = GenreModel(sequelize);
 
-
-// Relaciones de los modelos
-
-Videogame.belongsToMany(Genre, { through: 'videogame_genre' });
-Genre.belongsToMany(Videogame, { through: 'videogame_genre' });
-
+// Videogame.belongsToMany(Genre, { through: 'videogame_genre' });
+// Genre.belongsToMany(Videogame, { through: 'videogame_genre' });
 
 module.exports = {
-  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  Videogame,
+  Genre,
+  conn: sequelize,
 };
